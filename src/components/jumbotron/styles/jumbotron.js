18f0fd475e3899940ec5b8479268538181b17972
Darren Carrillo <48144231+darrenjcarrillo@users.noreply.github.com>
2020-10-30 14:13:04 -0700

    Jumbotron map function bug fixed

--- src/components/jumbotron/styles/jumbotron.js
+++ src/components/jumbotron/styles/jumbotron.js
@@ -3,15 +3,26 @@ import styled from 'styled-components/macro';
 export const Inner = styled.div`
   display: flex;
   align-items: center;
-  flex-direction: ${({ direction }) => direction};
   justify-content: space-between;
+  flex-direction: ${({ direction }) => direction};
   max-width: 1100px;
-  margin: 100%;
+  margin: auto;
   width: 100%;
 
   @media (max-width: 1000px) {
     flex-direction: column;
   }
 `;
+
+
+export const Pane = styled.div`
+  width: 50%;
+`;
+
+export const Title = styled.h1``;
+
+export const SubTitle = styled.h2``;
+
+export const Image = styled.img``;
 
 export const Container = styled.div``;
