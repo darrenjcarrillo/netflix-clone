e3251988f1b416a01b636e21396cf65298effecd
djosh <darrenjcarrillo@gmail.com>
2021-05-25 20:26:47 -0700

    Footer Links setup

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -20,7 +20,7 @@ export const Column = styled.div`
 
 export const Row = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(210px, 1fr));
     grid-gap: 15px
 
     @media (max-width: 1000px) {
@@ -31,7 +31,7 @@ export const Row = styled.div`
 export const Link = styled.a`
     color: #757575;
     margin-bottom: 20px;
-    font-size: 13px;
+    font-size: 14px;
     text-decoration: none;
 `;
 
@@ -50,7 +50,6 @@ export const Text = styled.p`
 export const Break = styled.p`
     flex-basis: 100%;
     height: 0;
-
 `;
 
 
