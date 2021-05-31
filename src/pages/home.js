1700f79745191d82e264f7694943c22fa5dd3f6a
djosh <darrenjcarrillo@gmail.com>
2021-05-31 09:08:25 -0700

    Header update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { OptForm } from '../components';
 import { HeaderContainer } from '../containers/header';
 import { FaqsContainer } from '../containers/faqs';
 import { FooterContainer } from '../containers/footer';
@@ -7,10 +8,17 @@ import { JumbotronContainer } from '../containers/jumbotron';
 export default function Home() {
     return (
         <>
-        <HeaderContainer />
-        <JumbotronContainer />
-        <FaqsContainer />
-        <FooterContainer />
+            <HeaderContainer>
+                <OptForm>
+                    <OptForm.Input placeholder="Email address" />
+                    <OptForm.Button>Try it now</OptForm.Button>
+                    <OptForm.Break />
+                    <OptForm.Text>Ready to watch? Enter your email to create or restart your membership</OptForm.Text>
+                </OptForm>
+            </HeaderContainer>
+            <JumbotronContainer />
+            <FaqsContainer />
+            <FooterContainer />
         </>
     )
 }
