dc7fbd388ecd5a9f16821e73d6efd755aad7e5c9
djosh <darrenjcarrillo@gmail.com>
2021-06-01 20:46:23 -0700

    Firebase initialized & connected

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'normalize.css';
 import { GlobalStyles } from './global-styles';
-
+import { firebase } from './lib/firebase.prod';
+ 
 ReactDOM.render(
   <>
     <GlobalStyles />
--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,13 +1,24 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { seedDatabase } from '../seed';
 
-// Need to somehow seed the database
+// seed the database
 
 
-// we need a config here
-const config = {};
+// config here
+const config = {
+    apiKey: "AIzaSyB4iylacSdxLH2pzAc5IxYHTX7bFT8wfF0",
+    authDomain: "netflix-clone-599a4.firebaseapp.com",
+    projectId: "netflix-clone-599a4",
+    storageBucket: "netflix-clone-599a4.appspot.com",
+    messagingSenderId: "486866070746",
+    appId: "1:486866070746:web:a10627d6c23ca869adc9e3"
+};
 
 const firebase = Firebase.initializeApp(config);
 
+// seedDatabase(firebase);
+
 export { firebase };
+
