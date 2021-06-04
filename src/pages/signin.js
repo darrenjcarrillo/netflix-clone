03e2bd0d7363fdc6d977667d562d3c09725a0cd5
djosh <darrenjcarrillo@gmail.com>
2021-06-04 14:14:01 -0700

    Sigin Connected to Firebase

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
 import { FooterContainer } from '../containers/footer';
 import { HeaderContainer } from '../containers/header';
 import { Form } from '../components';
+import * as ROUTES  from '../constants/routes';
+
 
 
 export default function Signin() {
+    const history = useHistory();
+    const { firebase } = useContext(FirebaseContext);
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
@@ -17,6 +23,18 @@ export default function Signin() {
         event.preventDefault();
         
         // Firebase works here!
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(emailAddress, password)
+            .then(() => {
+                // push to the browse page
+                history.push(ROUTES.BROWSE);
+            })
+            .catch((error) => {
+                setEmailAddress('');
+                setPassword('');
+                setError(error.message);
+            })
     }
 
 
