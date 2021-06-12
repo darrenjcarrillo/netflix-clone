3e205d1eb2a041be1116f7799b27e9bfa0f624a0
djosh <darrenjcarrillo@gmail.com>
2021-06-12 15:38:35 -0700

    User redirect when logged

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Home, Browse, Signin, Signup } from './pages';
 import * as ROUTES from './constants/routes';
+import { IsUserRedirect } from './helpers/routes';
+
 
 
 export default function App() {
+  const user = {};
+
   return (
     <Router>
-      <Route exact path="/signup">
-        <Signup />
+      <Route  path="/signin">
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
+          <Signin />
+        </IsUserRedirect>
       </Route>
 
-      <Route exact path="/signin">
-        <Signin />
+      <Route exact path="/signup">
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact>
+            <Signup />
+        </IsUserRedirect>
       </Route>
 
       <Route exact path="/browse">
